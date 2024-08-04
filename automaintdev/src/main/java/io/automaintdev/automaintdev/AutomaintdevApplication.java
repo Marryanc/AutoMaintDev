9acfc99c88a6ff417284d03fc3560130d2e3f3c7
Marryanc <ryan.marshall.me@gmail.com>
2024-08-04 08:00:14 -0400

    Initial Commit

---
+++ automaintdev/src/main/java/io/automaintdev/automaintdev/AutomaintdevApplication.java
@@ -0,0 +1,13 @@
+package io.automaintdev.automaintdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaintdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaintdevApplication.class, args);
+	}
+
+}
